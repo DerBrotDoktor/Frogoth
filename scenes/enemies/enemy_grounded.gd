b97a94013921deedc6c83cfbd793afc2a93b40d5
Leon Brodel <none@none>
2024-01-16 10:01:41 +0100

    Finished enemy grounded movement

--- scenes/enemies/enemy_grounded.gd
+++ scenes/enemies/enemy_grounded.gd
@@ -1,21 +1,28 @@
 extends CharacterBody2D
 
 
-@export var speed :int = 100
+@export var speed :int = 20
 @export var direction :int = 1
 
-@onready var flip_cooldown_timer :Timer = $FlipCooldownTimer
+@export var flip_cooldown_time :float = 0.3
+@onready var flip_cooldown_time_timer :Timer = $FlipCooldownTimer
 @onready var horizontal_ray_cast :RayCast2D = $HorizontalRayCast
 @onready var vertical_ray_cast_l :RayCast2D = $VerticalRayCastL
 @onready var vertical_ray_cast_r :RayCast2D = $VerticalRayCastR
 
+@onready var sprite :AnimatedSprite2D = $Animation
+
+
 func _physics_process(delta):
-	velocity.x = speed * direction * delta
-	print(velocity.x)
+	velocity.x = speed * direction
 	move_and_slide()
-	if not horizontal_ray_cast.is_colliding() or not vertical_ray_cast_l.is_colliding() or not vertical_ray_cast_r.is_colliding():
+	if (horizontal_ray_cast.is_colliding() or not vertical_ray_cast_l.is_colliding() or not vertical_ray_cast_r.is_colliding()) and flip_cooldown_time_timer.is_stopped():
 		flip()
 
+
 func flip():
+	flip_cooldown_time_timer.start(flip_cooldown_time)
+	print ("flip")
+	sprite.flip_h = !sprite.flip_h
 	direction *= -1
 	pass
