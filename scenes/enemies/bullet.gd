db0982b6ed87e49366ab3f5591012f1156b33adf
Leon Brodel <none@none>
2024-01-22 15:01:30 +0100

    Added bullets get destroyed when colliding with level border

--- scenes/enemies/bullet.gd
+++ scenes/enemies/bullet.gd
@@ -7,3 +7,10 @@ func _physics_process(_delta):
 	if direction:
 		velocity = direction * speed
 	move_and_slide()
+
+
+func _on_trigger_area_body_entered(body):
+	print(body)
+	if not body.is_in_group("player") and not body.is_in_group("bullet"):
+		queue_free()
+	pass
