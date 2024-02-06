e2ae38e6fb1a2b5f87833562c8da8e502fafba2b
Leon Brodel <none@none>
2024-02-06 15:21:45 +0100

    Changed: Kill shape code

--- scenes/kill_shape/kill_shape.gd
+++ scenes/kill_shape/kill_shape.gd
@@ -13,17 +13,18 @@ func finish_shape(new_points):
 	for point in new_points:
 		add_point(point)
 	$Outline.closed = true
+	var polygons = get_polygons(new_points)
+
+	print("__________________________")
+	for point_array in polygons:
+		print("ARRAY:",point_array)
+		var shape = Polygon2D.new()
+		shape.polygon = point_array
+		add_child(shape)
+		var coll = CollisionPolygon2D.new()
+		coll.polygon = point_array
+		$KillArea.add_child(coll)
 	
-	var decomposed_points = Geometry2D.decompose_polygon_in_convex(point_positions)
-	for point_array in decomposed_points:
-		var point_numbers = []
-		for point in point_array:
-			point_numbers.append(point_positions.find(point))
-		$PolygonSprite.polygons.append(point_numbers)
-	
-	
-	$PolygonSprite.polygon = point_positions
-	$KillArea/KillAreaCollision.polygon = point_positions
 	$DeleteTimer.start()
 
 func clear_points():
@@ -39,3 +40,58 @@ func delete_shape():
 
 func _on_delete_timer_timeout():
 	delete_shape()
+
+func get_polygons(polygon):
+	
+	var points = []
+	for pol in polygon:
+		points.append(pol.position)
+	var point_list = []
+	var result_list = []
+	var end_index = points.size()
+	
+	for i in range(0, end_index):
+		point_list.append(points[i])
+		if i+1 < points.size():
+			var a1 = points[i]
+			var a2 = points[i+1]
+			for j in range(i+2,end_index-1):
+				var b1 = points[j]
+				var b2 = points[j+1] if j+1 < points.size() else points[0]
+				var intersection = get_intersection(a1,a2,b1,b2)
+				if intersection == a1:
+					intersection = null
+				if intersection:
+					print("intersect",intersection)
+					point_list.append(intersection)
+					for h in range(j+1, end_index):
+						point_list.append(points[h])
+					result_list.append(point_list)
+					point_list = []
+					point_list.append(intersection)
+					end_index = j+2
+					i = j
+	print(point_list)
+	result_list.append(point_list)
+	return result_list
+
+func get_intersection(a1, a2, b1, b2):
+	var dir_a = a2-a1
+	var dir_a_perpendicular = Vector2(dir_a.y, -dir_a.x).normalized()
+	var to_b1 = b1-a1
+	var to_b2 = b2-a1
+	
+	if sign(dir_a_perpendicular.dot(to_b1)) == sign(dir_a_perpendicular.dot(to_b2)):
+		return null
+	
+	var dir_b = b2-b1
+	var dir_b_perpendicular = Vector2(dir_b.y, -dir_b.x).normalized()
+	var to_a1 = a1-b1
+	var to_a2 = a2-b1
+	
+	if sign(dir_b_perpendicular.dot(to_a1)) == sign(dir_b_perpendicular.dot(to_a2)):
+		return null
+	
+	var dist_b1 = abs(dir_a_perpendicular.dot(to_b1))
+	var dist_b2 = abs(dir_a_perpendicular.dot(to_b2))
+	return (dist_b2 * b1 + dist_b1 * b2) / (dist_b1 + dist_b2)
