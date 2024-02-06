a15e1cac6c8b38e6641139087e0f1258766b507a
Leon Brodel <none@none>
2024-02-06 09:30:06 +0100

    Added: Level select Back button

--- scenes/menus/level_select_menu.gd
+++ scenes/menus/level_select_menu.gd
@@ -9,3 +9,7 @@ func load_level(number:int):
 func _on_visibility_changed():
 	if $VerticalContainer/LevelMargin/LevelGrid/Level1Button.is_inside_tree() and visible:
 		$VerticalContainer/LevelMargin/LevelGrid/Level1Button.grab_focus()
+
+
+func _on_back_button_pressed():
+	get_parent().switch_to_child("MainMenu")
