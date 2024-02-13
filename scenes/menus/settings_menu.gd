7fcaa9eb7a7789ac29019c9b05b4585f4c266955
Leon Brodel <none@none>
2024-02-13 15:17:29 +0100

    Added: Window mode to settings menu

--- scenes/menus/settings_menu.gd
+++ scenes/menus/settings_menu.gd
@@ -1,5 +1,9 @@
 extends Control
 
+func _ready():
+	_on_master_slider_value_changed($MarginContainer/VBoxContainer/GridContainer/MasterSlider.value)
+	_on_music_slider_value_changed($MarginContainer/VBoxContainer/GridContainer/MusicSlider.value)
+	_on_sfx_slider_value_changed($MarginContainer/VBoxContainer/GridContainer/SFXSlider.value)
 
 func _on_master_slider_value_changed(value):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear_to_db(value))
@@ -17,3 +21,14 @@ func _on_sfx_slider_value_changed(value):
 
 func _on_back_button_pressed():
 	get_parent().switch_to_last_child()
+
+
+func _on_fullscreen_check_box_toggled(toggled_on):
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+
+func _on_borderless_checkbox_toggled(toggled_on):
+	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, toggled_on)
