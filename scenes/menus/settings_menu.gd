de2f6f257260d3338cffe6e4b688831baea1e22f
Leon Brodel <none@none>
2024-02-16 12:38:05 +0100

    Added: Thunder sfx Added: Thunder sfx bus

--- scenes/menus/settings_menu.gd
+++ scenes/menus/settings_menu.gd
@@ -19,16 +19,18 @@ func _on_sfx_slider_value_changed(value):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SoundEffects"), linear_to_db(value))
 	AudioServer.set_bus_mute(AudioServer.get_bus_index("SoundEffects"), value < 0.05)
 
+func _on_thunder_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("ThunderEffects"), linear_to_db(value))
+	AudioServer.set_bus_mute(AudioServer.get_bus_index("ThunderEffects"), value < 0.05)
+
 func _on_back_button_pressed():
 	get_parent().switch_to_last_child()
-
 
 func _on_fullscreen_check_box_toggled(toggled_on):
 	if toggled_on:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-
 
 func _on_borderless_checkbox_toggled(toggled_on):
 	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, toggled_on)
