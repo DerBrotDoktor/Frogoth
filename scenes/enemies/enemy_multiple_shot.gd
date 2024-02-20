dd87d7277b6e37d21f13c3151ad17d2dc18185ab
Leon Brodel <none@none>
2024-02-20 11:19:32 +0100

    Fixed: enemy 2 attack without target

--- scenes/enemies/enemy_multiple_shot.gd
+++ scenes/enemies/enemy_multiple_shot.gd
@@ -9,13 +9,14 @@ func attack():
 	play_animation("attack")
 
 func shoot():
-	var angle_per_bullet = angle / (bullet_amount - 1)
-	var player_angle = rad_to_deg(get_angle_to(target.global_position))
-	
-	for i in range(bullet_amount):
-		var bullet_angle = (-angle/2) + (angle_per_bullet * i)
-		var bullet_direction = player_angle + bullet_angle
-		super.new_bullet(Vector2.from_angle(deg_to_rad(bullet_direction)))
+	if is_instance_valid(target):
+		var angle_per_bullet = angle / (bullet_amount - 1)
+		var player_angle = rad_to_deg(get_angle_to(target.global_position))
+		
+		for i in range(bullet_amount):
+			var bullet_angle = (-angle/2) + (angle_per_bullet * i)
+			var bullet_direction = player_angle + bullet_angle
+			super.new_bullet(Vector2.from_angle(deg_to_rad(bullet_direction)))
 
 func play_animation(animation):
 	if not is_dead:
