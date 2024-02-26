53eac94a5d83f3e85bd219fa2a78ee8027e19588
Leon Brodel <none@none>
2024-02-26 17:37:32 +0100

    DO NOT UPDATE Added: tutorial sign sprite switch

--- scenes/enviroment/tutorial_sign/tutorial_sign.gd
+++ scenes/enviroment/tutorial_sign/tutorial_sign.gd
@@ -1,6 +1,26 @@
 extends Node2D
 
 
+enum INPUT_METHODS {KEYBOARD, CONTROLLER}
+
+var last_input_method :INPUT_METHODS
+
+func _input(event):
+	if event is InputEventJoypadButton and last_input_method == INPUT_METHODS.KEYBOARD:
+		last_input_method = INPUT_METHODS.CONTROLLER
+		update_key_prompts()
+	elif (event is InputEventKey or event is InputEventMouseButton) and last_input_method == INPUT_METHODS.CONTROLLER:
+		last_input_method = INPUT_METHODS.KEYBOARD
+		update_key_prompts()
+
+func update_key_prompts():
+	if last_input_method == INPUT_METHODS.KEYBOARD:
+		$TextContainer/KeyboardPrompts.visible = true
+		$TextContainer/ControllerPrompts.visible = false
+	elif last_input_method == INPUT_METHODS.CONTROLLER:
+		$TextContainer/KeyboardPrompts.visible = false
+		$TextContainer/ControllerPrompts.visible = true
+
 func _ready():
 	hide_text()
 
