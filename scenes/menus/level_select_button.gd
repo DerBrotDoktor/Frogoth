eb791b8b81f2bee7b1794ae6e64a7185c1c19127
Leon Brodel <none@none>
2024-03-01 16:37:52 +0100

    Changed: level select menu

--- scenes/menus/level_select_button.gd
+++ scenes/menus/level_select_button.gd
@@ -2,9 +2,13 @@ extends TextureButton
 
 signal load_level(number)
 @export var level_number :int
+@export var difficulty = 0
+@export var difficulty_sprites :SpriteFrames
 
 func _ready():
 	$Text.text = str(level_number)
+	if difficulty > 0:
+		$Difficulty.texture = difficulty_sprites.get_frame_texture("default", difficulty-1)
 
 func _on_pressed():
 	load_level.emit(level_number)
