4d90ed16dd5a0ecc9d1b737d77c27e4e13574b4f
Leon Brodel <none@none>
2024-03-04 16:09:53 +0100

    Fixed: paused end level

--- scenes/level/the_end.gd
+++ scenes/level/the_end.gd
@@ -3,6 +3,7 @@ extends "res://scenes/level/level.gd"
 func _ready():
 	super._ready()
 	get_tree().current_scene.remove_ui()
+	get_tree().paused = false
 
 func _process(_delta):
 	if Input.is_action_just_pressed("discrad_shape"):
